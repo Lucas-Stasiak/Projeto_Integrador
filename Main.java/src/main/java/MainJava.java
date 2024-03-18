feb294c9639e3e11783509fc0da8f5e24ce6fa75
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2024-03-18 11:28:24 -0300

    MEGA UPDATE
    Sout atualizado

--- Main.java/src/main/java/MainJava.java
+++ Main.java/src/main/java/MainJava.java
@@ -4,7 +4,7 @@ package main.java;
 
 public class MainJava {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ola mundo!");
     }
     
 }
